35c60e5385fdde5560dfeef909a1e8dbd5e0c6cb
Tomohiro Takahashi <ttakahashi@embarcadero.com>
2014-08-18 12:57:08 +0900

    add FMX Project

---
+++ Unit1.pas
@@ -0,0 +1,25 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+  private
+    { private éŒ¾ }
+  public
+    { public éŒ¾ }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
